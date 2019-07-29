f38389e9494d1c66f18ff5a88a98e4d430e1e620
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 08:02:04 -0700

    newmain

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-sjsj
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class WelcomePage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        index_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
+class SignupPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        aboutme_template = the_jinja_env.get_template('templates/sign_up.html')
+        self.response.write(sign_up_template.render())
+class ProfilePage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        profile_template = the_jinja_env.get_template('templates/profile.html')
+        self.response.write(profile_template.render())
+class MatchPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        match_template = the_jinja_env.get_template('templates/match.html')
+        self.response.write(match_template.render())
+class HomePage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        home_template = the_jinja_env.get_template('templates/home.html')
+        self.response.write(home_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', WelcomePage), #this maps the root url to the Main Page Handler
+    ('/signup', SignupPage),
+    ('/profile', ProfilePage),
+    ('/match', MatchPage),
+    ('/Home', HomePage)
+
+], debug=True)
